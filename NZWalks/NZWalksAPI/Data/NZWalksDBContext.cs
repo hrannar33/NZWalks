bf44526a0b091d900fdf19dc5f6b45a23152e669
Hrannar Hliddal Thorvaldsson <hrannar33@outlook.com>
2022-11-20 21:21:38 +0000

    Domains and DbContest

---
+++ NZWalks/NZWalksAPI/Data/NZWalksDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDBContext: DbContext
+    {
+        public NZWalksDBContext(DbContextOptions<NZWalksDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walk { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty{ get; set; }
+
+    }
+}
